e48601f1b67bc67b6a55c9ce05d92bb91c8813a7
Julia-Legrand <julia-legrand@orange.fr>
2023-11-23 11:05:40 +0100

    Mise à jour du code

--- portfolio/src/Entity/Skills.php
+++ portfolio/src/Entity/Skills.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\SkillsRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Projects;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\SkillsRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: SkillsRepository::class)]
 class Skills
