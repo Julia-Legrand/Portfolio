bea50167349ffd85f1a372738feb4bbf479143c2
Julia-Legrand <julia-legrand@orange.fr>
2023-12-01 23:09:40 +0100

    Suppression d'animation JS

--- portfolio/public/js/scripts.js
+++ portfolio/public/js/scripts.js
@@ -1,23 +1,3 @@
-// Animation to progressively reveal about section on scroll
-document.addEventListener('DOMContentLoaded', function () {
-    var elements = document.querySelectorAll('.aboutText, .aboutPicture');
-
-    function checkElements() {
-        elements.forEach(function (element) {
-            var elementTop = element.getBoundingClientRect().top;
-            var windowHeight = window.innerHeight;
-            var scroll = window.scrollY || window.pageYOffset;
-
-            if (scroll > elementTop - windowHeight + 100) {
-                element.classList.add('animated');
-            }
-        });
-    }
-
-    window.addEventListener('scroll', checkElements);
-    checkElements(); // Pour déclencher l'animation au chargement de la page
-});
-
 // Animation to progressively reveal timeline articles as you scroll
 document.addEventListener('DOMContentLoaded', function() {
     const timelineArticles = document.querySelectorAll('.timeline article');
