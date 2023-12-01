520783bc21ff900ec2a092ea08c0d04094be61e7
Julia-Legrand <julia-legrand@orange.fr>
2023-12-01 17:01:40 +0100

    Ajout d'animations JS

--- portfolio/public/js/scripts.js
+++ portfolio/public/js/scripts.js
@@ -1,3 +1,42 @@
+// Animation to progressively reveal about section on scroll
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the "about-section" section
+    var aboutSection = document.getElementById("about-section");
+    
+    // Get the elements
+    var aboutText = document.querySelector(".aboutText");
+    var aboutPicture = document.querySelector(".aboutPicture");
+
+    // Calculate the vertical start position of the effect
+    var startEffectPosition = aboutSection.offsetTop - window.innerHeight / 2;
+
+    // Calculate the vertical end position of the effect (e.g., half of the window height)
+    var endEffectPosition = startEffectPosition + window.innerHeight / 2;
+
+    // Function to move elements on scroll
+    function moveElementsOnScroll() {
+      // Get the scroll position
+      var scrollPosition = window.scrollY || window.pageYOffset;
+
+      // Apply transformations only when scrolling is between the start and end positions of the effect
+      if (scrollPosition > startEffectPosition && scrollPosition <= endEffectPosition) {
+        var translateY = (scrollPosition - startEffectPosition) / 4;
+
+        // Apply transformations
+        aboutText.style.transform = "translateX(" + translateY + "px)";
+        aboutPicture.style.transform = "translateX(-" + translateY + "px)";
+      } else if (scrollPosition <= startEffectPosition) {
+        // Reset transformations if scrolling is below the start position of the effect
+        aboutText.style.transform = "translateX(0)";
+        aboutPicture.style.transform = "translateX(0)";
+      }
+    }
+
+    // Call the function on initial load and on scroll
+    moveElementsOnScroll();
+    window.addEventListener("scroll", moveElementsOnScroll);
+});
+
 // Animation to progressively reveal timeline articles as you scroll
 document.addEventListener('DOMContentLoaded', function() {
     const timelineArticles = document.querySelectorAll('.timeline article');
